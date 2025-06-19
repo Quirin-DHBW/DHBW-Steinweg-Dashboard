b45ad11c86c6c5a9061c07d38f0682eb0d434652
Quirin62 <91368114+Quirin62@users.noreply.github.com>
2025-06-19 16:31:49 +0200

    I fixed a misaligned space. This is a critical push. Yes.

--- dash_prototyp.py
+++ dash_prototyp.py
@@ -218,7 +218,7 @@ def get_kostenart_fig(abteilung=None, jahr=None):
         x="Kategorie",
         y="ist_summe",
         barmode="group",
-        title=f"Kostenarten {'für ' + abteilung if abteilung != None else ''} (Ist-Werte)"
+        title=f"Kostenarten{' für ' + abteilung if abteilung != None else ''} (Ist-Werte)"
     )
 
     return fig
